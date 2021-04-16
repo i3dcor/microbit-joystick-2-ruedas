ead6d87179c7b422457c46ee27628fa7829723bb
i3dcor <i3dcor@gmail.com>
2021-04-16 13:23:28 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,29 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receiver_mode != 0) {
-        led.plotBrightness(0, 2, left_pwm)
-        led.plotBrightness(4, 2, right_pwm)
+        led.plotBrightness(0, 2, pins.map(
+        left_pwm,
+        0,
+        DAC_resolution - 1,
+        0,
+        LED_max_value
+        ))
+        led.plotBrightness(4, 2, pins.map(
+        right_pwm,
+        0,
+        DAC_resolution - 1,
+        0,
+        LED_max_value
+        ))
         if (receivedNumber < 0) {
             reverse = 1
         } else {
             reverse = 0
         }
-        left_pwm = Math.round(Math.abs(Math.trunc(receivedNumber / 1000)))
-        right_pwm = Math.round(Math.abs(receivedNumber) - left_pwm * 1000)
+        left_pwm = Math.round(Math.abs(Math.trunc(receivedNumber / decimals)))
+        right_pwm = Math.round(Math.abs(receivedNumber) - left_pwm * decimals)
+        pins.analogWritePin(AnalogPin.P0, left_pwm)
+        pins.analogWritePin(AnalogPin.P1, right_pwm)
+        pins.digitalWritePin(DigitalPin.P2, reverse)
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -73,7 +88,7 @@ function read_data () {
         left_pwm = accelY
         right_pwm = accelY
     }
-    direction_left_right_combined_number = right_pwm + left_pwm * 1000
+    direction_left_right_combined_number = right_pwm + left_pwm * decimals
     if (reverse != 0) {
         direction_left_right_combined_number = -1 * direction_left_right_combined_number
     }
@@ -87,25 +102,41 @@ let readX = 0
 let reverse = 0
 let ADC_deadzone_high = 0
 let ADC_deadzone_low = 0
+let LED_max_value = 0
 let ADC_resolution = 0
 let DAC_resolution = 0
 let right_pwm = 0
 let left_pwm = 0
+let decimals = 0
 let receiver_mode = 0
 radio.setGroup(99)
 receiver_mode = 0
+decimals = 10000
 left_pwm = 50
 right_pwm = 50
-DAC_resolution = 256
+DAC_resolution = 1024
 let deadzone_width = 24
 ADC_resolution = 1024
+LED_max_value = 255
 ADC_deadzone_low = ADC_resolution / 2 - deadzone_width / 2
 ADC_deadzone_high = ADC_resolution / 2 + deadzone_width / 2
 basic.forever(function () {
     if (receiver_mode == 0) {
         read_data()
-        led.plotBrightness(0, 2, left_pwm)
-        led.plotBrightness(4, 2, right_pwm)
+        led.plotBrightness(0, 2, pins.map(
+        left_pwm,
+        0,
+        DAC_resolution - 1,
+        0,
+        LED_max_value
+        ))
+        led.plotBrightness(4, 2, pins.map(
+        right_pwm,
+        0,
+        DAC_resolution - 1,
+        0,
+        LED_max_value
+        ))
         basic.pause(100)
     }
 })
